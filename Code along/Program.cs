01b86eb5e72c291ac463fd3048d3559c8364a640
Dennislindstrom <113332155+Dennislindstrom@users.noreply.github.com>
2022-09-12 08:46:42 +0200

    Initial commit

---
+++ Code along/Program.cs
@@ -0,0 +1,13 @@
+﻿//-----------------PSEUDOKOD---------------------
+
+// Welcome user to the app.
+// SHow available products to the user.
+// Show what is in the shopping cart to the user.
+// Ask the user for what they want to buy, or if they want to leave the store. 
+// IF the product the user want to buy is available.
+// Put the product in the shopping cart.
+// Remove the product from the available products.
+// ELSE
+// tell the user that the product is not available.
+
+Console.WriteLine($"Welcome to the store");
